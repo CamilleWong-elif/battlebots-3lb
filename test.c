5d04a858041d105683663b0e46a6192dff7383c0
CamilleWong-elif <133308115+CamilleWong-elif@users.noreply.github.com>
2026-02-02 17:14:15 -0800

    Update and rename test.py to test.c

---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    const char *DATA_PATH = "/content/drive/Shared drives/3lb Battlebot ASME CSULB";
+    printf("Files are in: %s\n", DATA_PATH);
+    return 0;
+}
